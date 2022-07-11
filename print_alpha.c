14324274c69e70964520274b2131cb0ff3576075
Chukwujama <Chukwujama>
2022-07-11 14:38:30 -0700

    Add

--- print_alpha.c
+++ print_alpha.c
@@ -8,19 +8,27 @@
  */
 int print_string(va_list args)
 {
-	int i;
 	char *str = va_arg(args, char *);
 
-	if (str == NULL)
-		str = "(null)";
-	else if (*str == '\0')
-		return (-1);
-
-	for (i = 0; str[i]; i++)
-		putchar(str[i]);
-
-	return (i);
+	if (!str)
+		s = "(null)";
+	return (puts(str));
 }
+
+/*
+ *	int i;
+ *	char *str = va_arg(args, char *);
+ *
+ *	if (str == NULL)
+ *		str = "(null)";
+ *	else if (*str == '\0')
+ *		return (-1);
+ *
+ *	for (i = 0; str[i]; i++)
+ *		putchar(str[i]);
+ *
+ *	return (i);
+ */
 
 /**
  * print_char - prints characters
