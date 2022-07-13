1d14d62bee7a469dac58d89c39c7711a71937a75
Chukwujama <Chukwujama>
2022-07-13 11:24:22 -0700

    Add

--- print_oct_bin.c
+++ print_oct_bin.c
@@ -1,5 +1,9 @@
 #include "main.h"
-
+/**
+ * print_octal - takes an unsigned int and prints it in binary notation
+ * @o: unsigned int to print
+ * Return: unsigned int to print
+ */
 int print_octal(va_list o)
 {
 	unsigned int a[11];
@@ -58,40 +62,3 @@ int print_binary(va_list b)
 	}
 	return (count);
 }
-
-/**
- * int main(void)
- * {
- *	unsigned int ui;
- *	long int res;
- *
- *	res = (long int)INT_MAX * 2;
- *	ui = (unsigned int)INT_MAX + 1024;
- *
- *	_printf("Unsigned octal:[%o]\n", ui);
- *	printf("Unsigned octal:[%o]\n", ui);
- *	_printf("%o\n", 0);
- *	printf("%o\n", 0);
- *	_printf("%o + %o = %o\n", INT_MAX, INT_MAX, res);
- *	printf("%o + %o = %o\n", INT_MAX, INT_MAX, res);
- *	_printf("%o - %o = %o\n", 2048, 1024, 1024);
- *	printf("%o - %o = %o\n", 2048, 1024, 1024);
- *	_printf("There is %o bytes in %o KB\n", 1024, 1);
- *	printf("There is %o bytes in %o KB\n", 1024, 1);
- *	_printf("%b + %b = %b\n", INT_MAX, INT_MAX, res);
- *	printf("%b + %b = %b\n", INT_MAX, INT_MAX, res);
- *	_printf("%b\n", 0);
- *	printf("%b\n", 0);
- *	_printf("There is %b bytes in %b KB\n", 1024, 1);
- *	printf("There is %b bytes in %b KB\n", 1024, 1);
- *	_printf("%b - %b = %b\n", 2048, 1024, 1024);
- *	printf("%b - %b = %b\n", 2048, 1024, 1024);
- *	_printf("%b\n", 98);
- *	printf("%b\n", 98);
- *	_printf("%u == %o == %x == %X\n", 1024, 1024, 1024, 1024);
- *	_printf("%x\n", 0);
- *	_printf("%X\n", 0);
- *
- *	return (0);
- *}
- */
